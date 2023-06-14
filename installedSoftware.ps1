0dab1ca06ba8356c0b1c06f9ad3cb806590b8684
Gihan Niroshan <69729026+gihanniroshan@users.noreply.github.com>
2023-06-14 13:27:24 +0530

    initial comit

---
+++ installedSoftware.ps1
@@ -0,0 +1,15 @@
+﻿$uninstallKeys = @(
+    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall',
+    'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+)
+
+$softwareList = foreach ($uninstallKey in $uninstallKeys) {
+    Get-ChildItem -Path $uninstallKey |
+        Get-ItemProperty |
+        Where-Object { $_.DisplayName -and $_.UninstallString } |
+        Select-Object DisplayName, DisplayVersion, Publisher
+}
+
+$sortedList = $softwareList | Sort-Object DisplayName
+
+$sortedList | Format-Table -AutoSize
